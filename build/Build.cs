82dade824f1f166ea4ef7e7d3960701093c3763f
Nathan Risto <nlristo@taylordigital.io>
2021-12-22 17:04:00 -0600

    pack rider differently

--- build/Build.cs
+++ build/Build.cs
@@ -1,6 +1,9 @@
+using System.IO;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Core;
 using Nuke.Core.Utilities.Collections;
+using static System.Console;
+using static System.Environment;
 using static System.IO.Compression.ZipFile;
 using static System.IO.Directory;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -22,6 +25,7 @@ class Build : NukeBuild
    // [Parameter] readonly string MyGetApiKey;
    // Returns command-line arguments and environment variables.
    public override AbsolutePath ArtifactsDirectory => SolutionDirectory / "packages";
+   string Version => "2021.3.1.1";
 
    Target Clean =>
       _ => _.Executes(() =>
@@ -42,7 +46,10 @@ class Build : NukeBuild
                       {
                          // TODO: DeleteDirectory not work, and move to Clean
                          if (Exists(ArtifactsDirectory))
+                         {
+                            WriteLine($@"Delete ""{ArtifactsDirectory}""{NewLine}");
                             Delete(ArtifactsDirectory, true);
+                         }
 
                          DotNetPack(_ => DefaultDotNetPack.SetOutputDirectory(ArtifactsDirectory)
                                                           .DisableIncludeSymbols()
@@ -52,7 +59,32 @@ class Build : NukeBuild
                                                           .DisableIncludeSymbols()
                                                           .SetProject("AsyncApostle/AsyncApostle.Rider.csproj"));
 
-                         CreateFromDirectory(SolutionDirectory / "Rider", ArtifactsDirectory / "AsyncApostle.Rider.zip");
+                         WriteLine($@"Unzip ""{SolutionDirectory / "Rider" / "AsyncApostle.Rider" / $"AsyncApostle.Rider.{Version}.nupkg"}"" to ""{ArtifactsDirectory / $"AsyncApostle.Rider.{Version}"}""{NewLine}");
+                         ExtractToDirectory(SolutionDirectory / "Rider" / "AsyncApostle.Rider" / $"AsyncApostle.Rider.{Version}.nupkg", ArtifactsDirectory / $"AsyncApostle.Rider.{Version}");
+
+                         WriteLine($@"Delete {SolutionDirectory / "Rider" / "AsyncApostle.Rider" / $"AsyncApostle.Rider.{Version}.nupkg"}{NewLine}");
+                         File.Delete(SolutionDirectory / "Rider" / "AsyncApostle.Rider" / $"AsyncApostle.Rider.{Version}.nupkg");
+
+                         WriteLine($@"Create ""{ArtifactsDirectory / "Rider"}""{NewLine}");
+                         CreateDirectory(ArtifactsDirectory / "Rider");
+
+                         WriteLine($@"Move ""{ArtifactsDirectory / $"AsyncApostle.Rider.{Version}" / "lib"}"" to ""{ArtifactsDirectory / "Rider" / "AsyncApostle.Rider"}""{NewLine}");
+                         Move(ArtifactsDirectory / $"AsyncApostle.Rider.{Version}" / "lib", ArtifactsDirectory / "Rider" / "AsyncApostle.Rider");
+
+                         WriteLine($@"Delete ""{ArtifactsDirectory / $"AsyncApostle.Rider.{Version}"}""{NewLine}");
+                         Delete(ArtifactsDirectory / $"AsyncApostle.Rider.{Version}", true);
+
+                         WriteLine($@"Create ""{ArtifactsDirectory / "Rider" / "AsyncApostle.Rider" / "lib"}""{NewLine}");
+                         CreateDirectory(ArtifactsDirectory / "Rider" / "AsyncApostle.Rider" / "lib");
+
+                         WriteLine($@"Zip ""{SolutionDirectory / "Rider" / "AsyncApostle.Rider"}"" into ""{ArtifactsDirectory / "Rider" / "AsyncApostle.Rider" / "lib" / $"AsyncApostle.Rider-{Version}.jar"}""{NewLine}");
+                         CreateFromDirectory(SolutionDirectory / "Rider" / "AsyncApostle.Rider", ArtifactsDirectory / "Rider" / "AsyncApostle.Rider" / "lib" / $"AsyncApostle.Rider-{Version}.jar");
+
+                         WriteLine($@"Zip ""{ArtifactsDirectory / "Rider"}"" into ""{ArtifactsDirectory / "AsyncApostle.Rider.zip"}""{NewLine}");
+                         CreateFromDirectory(ArtifactsDirectory / "Rider", ArtifactsDirectory / "AsyncApostle.Rider.zip");
+
+                         WriteLine($@"Delete ""{ArtifactsDirectory / "Rider"}""{NewLine}");
+                         Delete(ArtifactsDirectory / "Rider", true);
                       });
 
    Target Restore =>
