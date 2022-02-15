037965b3003fb9a0409a97985f2bbe45b5144f36
mu88 <4560672+mu88@users.noreply.github.com>
2022-02-15 13:34:36 +0100

    Ensure expression gets only highlighted once

--- src/dotnet/AsyncApostle/Analyzers/MissingAwaitAnalyzer.cs
+++ src/dotnet/AsyncApostle/Analyzers/MissingAwaitAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using AsyncApostle.AsyncHelpers.MissingAwaitChecker;
+﻿using System.Collections.Generic;
+using AsyncApostle.AsyncHelpers.MissingAwaitChecker;
 using AsyncApostle.Helpers;
 using AsyncApostle.Highlightings;
 using JetBrains.ProjectModel;
@@ -14,6 +15,8 @@ public class MissingAwaitAnalyzer : ElementProblemAnalyzer<IParametersOwnerDecla
 {
    #region methods
 
+   private readonly List<IInvocationExpression> _alreadyHighlightedExpressions = new();
+
    protected override void Run(IParametersOwnerDeclaration element, ElementProblemAnalyzerData data,
       IHighlightingConsumer consumer)
    {
@@ -21,8 +24,17 @@ public class MissingAwaitAnalyzer : ElementProblemAnalyzer<IParametersOwnerDecla
 
       foreach (var cSharpTreeNode in element.DescendantsInScope<ICSharpTreeNode>())
          if (missingAwaitChecker.AwaitIsMissing(cSharpTreeNode))
-            consumer.AddHighlighting(
-               new MissingAwaitHighlighting(cSharpTreeNode.GetContainingNode<IInvocationExpression>()!));
+            AddHighlightOnce(consumer, cSharpTreeNode.GetContainingNode<IInvocationExpression>()!);
+
+      _alreadyHighlightedExpressions.Clear();
+   }
+
+   private void AddHighlightOnce(IHighlightingConsumer consumer, IInvocationExpression invocationExpression)
+   {
+      if (_alreadyHighlightedExpressions.Contains(invocationExpression)) return;
+
+      _alreadyHighlightedExpressions.Add(invocationExpression);
+      consumer.AddHighlighting(new MissingAwaitHighlighting(invocationExpression));
    }
 
    #endregion
