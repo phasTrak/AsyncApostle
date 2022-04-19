0e8aa83d8c32e9b136303ae2fca791e906f8ef6b
Nathan Risto <nlristo@taylordigital.io>
2022-04-19 09:41:22 -0500

    update to 22.1

--- build/Build.cs
+++ build/Build.cs
@@ -25,7 +25,7 @@ class Build : NukeBuild
    // [Parameter] readonly string MyGetApiKey;
    // Returns command-line arguments and environment variables.
    public override AbsolutePath ArtifactsDirectory => SolutionDirectory / "packages";
-   string Version => "2021.3.4";
+   string Version => "2022.1.0";
 
    Target Clean =>
       _ => _.Executes(() =>
